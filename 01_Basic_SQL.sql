b2bc0f43d1d5102ddfd661507b62bb3f8e723697
K Bisoi <44413283+khetrobasi@users.noreply.github.com>
2026-06-29 00:57:24 +0530

    Add Basic SQL queries

---
+++ 01_Basic_SQL.sql
@@ -0,0 +1,29 @@
+/*
+====================================================
+Topic    : Basic SQL
+Database : CaseStudy_1
+Author   : K. Bisoi
+====================================================
+*/
+-- Tables Used
+-- Emp
+-- Department
+-- Job
+-- Location
+----------------------------------------------------
+-- 1. Display all employee records
+SELECT *
+FROM Emp;
+----------------------------------------------------
+-- 2. Display all department records
+SELECT *
+FROM Department;
+----------------------------------------------------
+-- 3. Display all job records
+SELECT *
+FROM Job;
+----------------------------------------------------
+-- 4. Display all location records
+SELECT *
+FROM Location;
+----------------------------------------------------
